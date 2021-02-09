3e10805f04a8ac1b32abdb31b6b327bf6b785d31
MdAshiks <mdashik8900@gmail.com>
2021-02-09 15:07:56 +0530

    index

--- contact.php
+++ contact.php
@@ -1,22 +1,13 @@
-<?php
-//variable setting
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$message = $_REQUEST['message'];
-
-
-//check input fields
-
-
-if (empty($name)|| empty($email) || empty($message)) {
-	echo "please fill all the fields";
-	}
-	else{
-		mail("mdashik8900@gmail.com", "demo", $message, "From: $name <$email>");
-		echo "<script type='text/javascript'>
-		alert('your message sent successfully');
-		window.history.log(-1);
-
-		</script>";
-	}
-?>
+<?php // Check if form was submitted
+if ($_POST['email'] && $_POST['message']) {
+  if ($_POST['subject']) {
+    echo "Go away you dirty spammer!";
+  } else {
+    mail(
+      "mdashik8900@gmail.com",
+      "[website] New message from ".$_POST['email'],
+      $_POST['message']
+    );
+    echo "<p>Thank you for your message</p>";
+  }
+}	?>
