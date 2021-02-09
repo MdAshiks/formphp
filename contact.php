b26fae34f8048656d3d473ffb571cff3f463a040
MdAshiks <mdashik8900@gmail.com>
2021-02-09 15:18:40 +0530

    inderfg

--- contact.php
+++ contact.php
@@ -9,16 +9,16 @@ $message = $_REQUEST['message'];
 
 
 if (empty($name)|| empty($email) || empty($message)) {
-	echo "please fill all the fields";
+	echo "<script type='text/javascript'> alert ('please fill all the fields')";
 	}
 	else{
-		
+
 		mail("mdashik8900@gmail.com", "demo", $message, "From: $name <$email>");
 
 
 		echo "<script type='text/javascript'>
 		alert('your message sent successfully');
-		window.history.log(-1);
+	
 
 		</script>";
 	}
