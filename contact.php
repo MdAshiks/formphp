770ce7e5dbe1022f013171d14707f4498bd87c70
MdAshiks <mdashik8900@gmail.com>
2021-02-09 14:35:46 +0530

    newlychanged

--- contact.php
+++ contact.php
@@ -1,16 +1,15 @@
 <?php
 //variable setting
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$message = $_REQUEST['message'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
 
 //check input fields
 
 
 if (empty($name)|| empty($email) || empty($message)) {
-	echo "<script type='text/javascript'>
-		alert('please fill all the fields')";
+	echo "please fill all the fields";
 	}
 	else{
 		mail("mdashik8900@gmail.com", "demo", $message, "From: $name <$email>");
